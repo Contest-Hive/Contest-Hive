5034f956899f31aefaf889279a5dfd3753fdd6c7
Nusab Taha <nusabtaha33@gmail.com>
2023-08-30 05:52:06 +0600

    why is og not working

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,6 +14,19 @@ export const metadata = {
     "contest, contest hive, contest api, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard",
 
   favicon: "favicon.ico",
+  openGraph: {
+    title: "Contest Hive",
+    description:
+      "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
+    image: "/assets/images/contestHive.png",
+    url: "https://contest-hive.vercel.app/",
+  },
+
+  twitterCard: "Contest Hive - Twitter",
+  twitterTitle: "Contest Hive",
+  twitterDescription:
+    "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
+  twitterImage: "/assets/images/contestHive.png",
 };
 
 const layout = ({ children }) => {
@@ -30,6 +43,7 @@ const layout = ({ children }) => {
         <meta name="twitter:image:width" content="<generated>" />
         <meta name="twitter:image:height" content="<generated>" />
       </Head>
+
       <body className="bg-gray-900 text-gray-200">
         <NavBar />
         {children}
