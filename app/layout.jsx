f5954def6985b3e39321c73eedc1b0b6be968dcd
Nusab Taha <nusabtaha33@gmail.com>
2023-08-30 05:49:49 +0600

    og head

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import "./globals.css";
 import Head from "next/head";
 
-const metadata = {
+export const metadata = {
   title: "Contest Hive",
   description:
     "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
@@ -14,57 +14,21 @@ const metadata = {
     "contest, contest hive, contest api, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard",
 
   favicon: "favicon.ico",
-  openGraph: {
-    title: "Contest Hive",
-    description:
-      "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
-    image: "/assets/images/contestHive.png",
-    url: "https://contest-hive.vercel.app/",
-  },
-
-  twitterCard: "Contest Hive - Twitter",
-  twitterTitle: "Contest Hive",
-  twitterDescription:
-    "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
-  twitterImage: "/assets/images/contestHive.png",
 };
 
 const layout = ({ children }) => {
   return (
     <html lang="en">
       <Head>
-        <title>Contest Hive</title>
-        <meta
-          name="description"
-          content="Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same"
-        />
-        <meta property="og:url" content="https://contest-hive.vercel.app/" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Contest Hive" />
-        <meta
-          property="og:description"
-          content="Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same"
-        />
-        <meta
-          property="og:image"
-          content="https://contest-hive.vercel.app/assets/images/contestHive.png"
-        />
+        <meta property="og:image" content="<generated>" />
+        <meta property="og:image:type" content="<generated>" />
+        <meta property="og:image:width" content="<generated>" />
+        <meta property="og:image:height" content="<generated>" />
 
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta property="twitter:domain" content="contest-hive.vercel.app" />
-        <meta
-          property="twitter:url"
-          content="https://contest-hive.vercel.app/"
-        />
-        <meta name="twitter:title" content="Contest Hive" />
-        <meta
-          name="twitter:description"
-          content="Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same"
-        />
-        <meta
-          name="twitter:image"
-          content="https://contest-hive.vercel.app/assets/images/contestHive.png"
-        />
+        <meta name="twitter:image" content="<generated>" />
+        <meta name="twitter:image:type" content="<generated>" />
+        <meta name="twitter:image:width" content="<generated>" />
+        <meta name="twitter:image:height" content="<generated>" />
       </Head>
       <body className="bg-gray-900 text-gray-200">
         <NavBar />
