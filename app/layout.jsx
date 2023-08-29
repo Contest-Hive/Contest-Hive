b7d4a2b178689c871a810e0ae7e3beb399325a33
Nusab Taha <nusabtaha33@gmail.com>
2023-08-29 21:32:38 +0600

    meh...

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,13 +12,16 @@ export const metadata = {
     "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
   keywords:
     "contest, contest hive, contest api, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard",
-  // viewport: "width=device-width, initial-scale=1.0",
+
   favicon: "favicon.ico",
-  ogTitle: "Contest Hive",
-  ogDescription:
-    "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
-  ogImage: "/assets/images/contestHive.png",
-  ogUrl: "https://contest-hive.vercel.app/",
+  openGraph: {
+    title: "Contest Hive",
+    description:
+      "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same",
+    image: "/assets/images/contestHive.png",
+    url: "https://contest-hive.vercel.app/",
+  },
+
   twitterCard: "Contest Hive - Twitter",
   twitterTitle: "Contest Hive",
   twitterDescription:
