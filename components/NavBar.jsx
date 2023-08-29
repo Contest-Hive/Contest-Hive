8c8a5c0c3f29315c9f052eb792804f1f68020dab
Nusab Taha <nusabtaha33@gmail.com>
2023-08-29 10:26:00 +0600

    navbar....

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -31,7 +31,7 @@ export const NavBar = () => {
   }
 
   return (
-    <nav className="bg-gray-900 border-gray-200">
+    <nav className="bg-gray-900">
       <div className="top-0 flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center">
           <Image
@@ -41,7 +41,7 @@ export const NavBar = () => {
             className="h-10 mr-3"
             alt={`${navTitle} Logo`}
           ></Image>
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+          <span className="self-center text-2xl font-semibold whitespace-nowrap text-white">
             {navTitle}
           </span>
         </a>
@@ -51,7 +51,7 @@ export const NavBar = () => {
           <button
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark: dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden focus:outline-none focus:ring-2 hover:bg-gray-700 focus:ring-gray-600"
             aria-controls="navbar-default"
             aria-expanded="false"
             onClick={handleNavbar}
@@ -77,11 +77,11 @@ export const NavBar = () => {
 
           {/* DropDown Links */}
           <div className={navbarOpen ? classWhenOpen : classWhenClosed}>
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 md:bg-gray-900 md:border-hidden border border-gray-700 rounded-lg md:flex-row md:space-x-8 md:mt-0 bg-gray-800">
               <li>
                 <a
                   href={blueLink.url}
-                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+                  className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0"
                   aria-current="page"
                 >
                   {blueLink.name}
@@ -92,7 +92,7 @@ export const NavBar = () => {
                 <li key={key}>
                   <a
                     href={navLinks[key]}
-                    className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                    className="block py-2 pl-3 pr-4 rounded md:hover:bg-transparentmd:hover:text-blue-700 md:p-0 hover:text-white"
                   >
                     {key}
                   </a>
