7b5bdf00b7b6f100898509bed0e6a58adfdd8961
Nusab Taha <nusabtaha33@gmail.com>
2023-09-01 03:30:51 +0600

    just push

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = (module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/api/atcoder", // Change this to match your API route
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // Change this to the origin(s) you want to allow
+          },
+        ],
+      },
+    ];
+  },
+});
 
-module.exports = nextConfig
+module.exports = nextConfig;
