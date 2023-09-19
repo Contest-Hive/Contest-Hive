99bee620cc911dca15f5ace9826c9c057cd1bfd9
Nusab Taha <nusabtaha33@gmail.com>
2023-09-20 02:28:10 +0600

    ok: og image

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,26 @@
 // Vercel Analytics
 // import { Analytics } from "@vercel/analytics/react";
-import "./globals.css";
+import "@/app/globals.css";
 
-// Using `export const metadata` has some problems loading OpenGraph image
-// So, using `meta` tags explicitly
-
+const title = "Contest Hive";
 const description =
   "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same";
 const keywords =
   "contest, contest hive, contest api, upcoming contest, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard";
-const title = "Contest Hive";
 const url = "https://contest-hive.vercel.app/";
-const image = "/assets/images/opengraph-image.png";
 
 export const metadata = {
-  title: "Acme",
-
+  title: title,
+  description: description,
+  keywords: keywords,
+  favicon: "favicon.ico",
+  openGraph: {
+    title: title,
+    description: description,
+    url: url,
+  },
 };
+
 const layout = ({ children }) => {
   return (
     <html lang="en">
