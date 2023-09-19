8a2f132e69b777ccd2c70b4a217f1f1d6aabdb50
Nusab Taha <nusabtaha33@gmail.com>
2023-09-20 00:05:16 +0600

    changing things in `/api`

--- app/api/main.py
+++ app/api/main.py
@@ -11,6 +11,7 @@ data = {
         "toph"
     ],
     "data": {
+        # "all": "all",
         "1": "atcoder",
         "2": "codechef",
         "3": "codeforces",
@@ -24,20 +25,39 @@ data = {
 
 def getText(name):
     text = """
+// import { NextResponse } from "next/server";
+
+import { getSecondsDifference } from "@/components/helpers/KontestsHelper";
+
 const API_URL = "https://raw.githubusercontent.com/Nusab19/__contest-hive-backend/main/cache/Data/"""+name+""".json";
 
-export async function GET() {
-	const response = await fetch(API_URL, {
-	next: {
-	  revalidate: 5 * 60, // 5 Minutes
-	},
-	});
-	
-	const data = await response.json();
-	return new Response(JSON.stringify(data, null, 4));
-}
 
-	""".strip()
+export async function GET() {
+  const response = await fetch(API_URL, {
+    next: {
+      revalidate: 5 * 60, // 5 Minutes
+    },
+  });
+
+  const data = await response.json();
+  const allContests = data.data;
+  const contests = [];
+  for (let i = 0; i < allContests.length; i++) {
+    const contest = allContests[i];
+    if (getSecondsDifference(contest.startTime) < 0) continue;
+    contests.push({ ...contest });
+  }
+  data.data = contests;
+
+  return new Response(JSON.stringify(data, null, 2), {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET",
+      "Access-Control-Allow-Headers": "*",
+    },
+  });
+}
+""".strip()
 
     return text
 
