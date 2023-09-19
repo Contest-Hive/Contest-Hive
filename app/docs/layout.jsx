0290be2a767cb528cdbcd7de2b6cd5b161cc1ed6
Nusab Taha <nusabtaha33@gmail.com>
2023-09-20 03:33:19 +0600

    meta tags for `/docs`

--- app/docs/layout.jsx
+++ app/docs/layout.jsx
@@ -1,5 +1,22 @@
 import SideBar from "@/components/docs/SideBar";
 
+const title = "Docs | Contest Hive";
+const description =
+  "Documentation for Contest Hive, a web app that shows upcoming contests from various platforms.";
+const keywords =
+  "contest, contest hive, contest hive docs, contest api docs, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard";
+
+export const metadata = {
+  title: title,
+  description: description,
+  keywords: keywords,
+  favicon: "favicon.svg",
+  openGraph: {
+    title: title,
+    description: description,
+  },
+};
+
 const layout = ({ children }) => {
   return (
     <main>
