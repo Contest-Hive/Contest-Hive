75decb16204be5eaa742026be407405039bef0bc
Nusab Taha <nusabtaha33@gmail.com>
2023-09-21 00:45:11 +0600

    description update

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,7 +4,7 @@ import "@/app/globals.css";
 
 const title = "Contest Hive";
 const description =
-  "Contest Hive provides contest information from various competitive programming platforms. It also provides a REST API for the same";
+  "Contests at Your Fingertips. All upcoming contests from 7 platforms in one place";
 const keywords =
   "contest, contest hive, contest api, upcoming contest, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard";
 const url = "https://contest-hive.vercel.app/";
