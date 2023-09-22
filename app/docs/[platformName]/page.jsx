a44747507159866127be5d4b98a6e05082d79ada
Nusab Taha <nusabtaha33@gmail.com>
2023-09-23 01:58:38 +0600

    redirect to 404 page from `/docs`

--- app/docs/[platformName]/page.jsx
+++ app/docs/[platformName]/page.jsx
@@ -1,9 +1,25 @@
 import GetPlatformPage from "@/components/docs/GetPlatformPage";
+import { notFound } from "next/navigation";
+
+const platforms = [
+  "all",
+  "atcoder",
+  "codechef",
+  "codeforces",
+  "hackerearth",
+  "hackerrank",
+  "leetcode",
+  "toph",
+];
 
 const page = ({ params }) => {
+  const platformName = String(params.platformName).toLowerCase();
+  if (!platforms.includes(platformName)) {
+    return notFound();
+  }
   return (
     <>
-      <GetPlatformPage platformName={params.platformName} />
+      <GetPlatformPage platformName={platformName} />
     </>
   );
 };
