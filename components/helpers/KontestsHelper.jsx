6b0f704ef5b91d1457d539463811083b852ef73e
Nusab Taha <nusabtaha33@gmail.com>
2023-09-23 09:39:12 +0600

    tooltip for `startingIn` td

--- components/helpers/KontestsHelper.jsx
+++ components/helpers/KontestsHelper.jsx
@@ -212,7 +212,7 @@ function getTable(data, platform, isDesktop) {
         <td className="px-6 py-4 font-medium">{pascalNames[platform]}</td>
         <td className="px-6 py-4 font-medium">No Contests Available</td>
         <td className="px-6 py-4">never</td>
-        <td className="px-6 py-4">-1</td>
+        <td className="px-6 py-4">0</td>
         <td className="px-6 py-4">Nowhere!</td>
       </tr>
     );
@@ -240,8 +240,23 @@ function getTable(data, platform, isDesktop) {
         >
           {trimString(contestName, maxLen)}
         </th>
-        <td className="px-6 py-4" title={startTime}>
-          {startingIn}
+        <td className="px-6 py-4">
+          <span id="startTime">{startingIn}</span>
+          <span
+            className="tooltip absolute mx-auto -mt-1 ml-5 inline-block w-56 select-none rounded-lg bg-slate-800 px-0 pb-[7px] pt-[5px] text-center text-sm font-medium text-gray-200 opacity-0 transition-opacity duration-200 md:w-64"
+            id="tooltip"
+          >
+            <svg
+              className="absolute -ml-3 mt-[3px] h-4 w-4 text-gray-800"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 10 16"
+            >
+              <path d="M8.766.566A2 2 0 0 0 6.586 1L1 6.586a2 2 0 0 0 0 2.828L6.586 15A2 2 0 0 0 10 13.586V2.414A2 2 0 0 0 8.766.566Z" />
+            </svg>
+            {startTime}
+          </span>
         </td>
         <td className="px-6 py-4" title={duration}>
           {duration}
