324d129967ac1b66f6ca7624cc5ff5578bb83ccf
Nusab Taha <nusabtaha33@gmail.com>
2023-09-24 11:23:22 +0600

    typo fix

--- components/AnimatedComponents.jsx
+++ components/AnimatedComponents.jsx
@@ -3,6 +3,11 @@ import { useEffect } from "react";
 import AOS from "@/others/applyAOS.js";
 import "@/others/aos.css";
 
+import Features from "@/components/Features";
+import UserExperience from "@/components/UserExperience";
+import Content from "@/components/Content";
+import ContactUs from "@/components/ContactUs";
+
 const AnimatedComponents = () => {
   useEffect(() => {
     {
