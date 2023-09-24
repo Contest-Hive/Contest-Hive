6c4f9ebbc91871513de89f1fd4c752205c80d0b7
Nusab Taha <nusabtaha33@gmail.com>
2023-09-24 09:41:21 +0600

    move animated client side components to another parent component

--- app/page.jsx
+++ app/page.jsx
@@ -1,34 +1,9 @@
-"use client";
-import { useEffect } from "react";
-import AOS from "@/others/applyAOS.js";
-import "@/others/aos.css";
-
-// ------------------------------
-
 import NavBar from "@/components/NavBar";
 import Hero from "@/components/Hero";
 import Kontests from "@/components/Kontests";
-import Features from "@/components/Features";
-import UserExperience from "@/components/UserExperience";
-import Content from "@/components/Content";
-import ContactUs from "@/components/ContactUs";
+import AnimatedComponents from "@/components/AnimatedComponents";
 import Footer from "@/components/Footer";
 
-const AnimatedComponents = () => {
-  useEffect(() => {
-    {
-      AOS.init({});
-    }
-  });
-  return (
-    <>
-      <Features />
-      <UserExperience />
-      <Content />
-      <ContactUs />
-    </>
-  );
-};
 
 const page = () => {
   return (
--- components/AnimatedComponents.jsx
+++ components/AnimatedComponents.jsx
@@ -3,17 +3,20 @@ import { useEffect } from "react";
 import AOS from "@/others/applyAOS.js";
 import "@/others/aos.css";
 
-const Animate = () => {
+const AnimatedComponents = () => {
   useEffect(() => {
     {
       AOS.init({});
     }
   });
   return (
-    <div data-aos="fade-up-right" className="h-32 w-32 bg-blue-500">
-      Hello World
-    </div>
+    <>
+      <Features />
+      <UserExperience />
+      <Content />
+      <ContactUs />
+    </>
   );
 };
 
-export default Animate;
+export default AnimatedComponents;
