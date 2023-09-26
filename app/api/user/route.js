d11e1712ed6f8bbd3ad484c892fd752313490ec9
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 02:54:04 +0600

    test

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -3,9 +3,16 @@ import requestIp from "request-ip";
 
 export async function GET(req) {
   const detectedIp = requestIp.getClientIp(req);
+  const forwarded = req.headers["x-forwarded-for"];
+
+  const ip =
+    typeof forwarded === "string"
+      ? forwarded.split(/, /)[0]
+      : req.socket.remoteAddress;
+
   const data = {
     ok: true,
-    ip: detectedIp,
+    ip,
     userAgent: req.headers.get("user-agent"),
   };
   return new NextResponse(JSON.stringify(data, null, 2));
