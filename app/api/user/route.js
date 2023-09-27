23c96d46f198450afdcb8337372ddb9e7f09b789
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 07:26:34 +0600

    [api] test

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,7 +1,17 @@
-import { NextResponse } from "next/server";
-export async function GET() {
+import { NextRequest, NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const { ip, nextUrl } = req;
+
+  nextUrl.searchParams.set("clientIp", ip);
+
+  return NextResponse.rewrite(nextUrl);
+}
+
+export async function GET(req) {
   const data = {
     ok: true,
+    ip: req.ip ?? "127.0.0.1",
     message: "Hello from the API",
   };
   return new NextResponse(JSON.stringify(data, null, 2));
