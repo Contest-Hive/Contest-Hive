f8fd994f19de8bbe1b625c4a1193d2e20d6217eb
Nusab Taha <nusabtaha33@gmail.com>
2023-09-27 09:55:09 +0600

    last test

--- app/api/user/route.js
+++ app/api/user/route.js
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextResponse } from 'next/server';
 
-export async function middleware(req: NextRequest) {
+export async function middleware(req) {
   const { ip, nextUrl } = req;
 
   nextUrl.searchParams.set('clientIp', ip);
