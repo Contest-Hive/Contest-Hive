3d49bb360a6e1bf13f0eecd123a228f068f603cf
Nusab Taha <nusabtaha33@gmail.com>
2023-09-30 03:32:03 +0600

    metadata export in bottom

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,24 @@
 import UnderMaintenance from "@/components/UnderUnderMaintenance";
 import "@/app/globals.css";
 
-
 // Change When Maintaining
 const IS_MAINTENANCE = false;
 
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body className="bg-gray-950 text-gray-200">
+        <main>{IS_MAINTENANCE ? <UnderMaintenance /> : children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default layout;
 
 const title = "Contest Hive";
 const description =
-  "Contests at Your Fingertips. All upcoming contests from 7 platforms in one place";
+  "Contests at Your Fingertips. All upcoming contests at one place. Never miss a contest again.";
 const keywords =
   "contest, contest hive, contest api, upcoming contest, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard";
 const url = "https://contest-hive.vercel.app/";
@@ -23,15 +33,3 @@ export const metadata = {
     url: url,
   },
 };
-
-const layout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body className="bg-gray-950 text-gray-200">
-        <main>{IS_MAINTENANCE ? <UnderMaintenance /> : children}</main>
-      </body>
-    </html>
-  );
-};
-
-export default layout;
