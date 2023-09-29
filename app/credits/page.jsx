6449ed694ed958d361fb97f5cf2c98fa3bb4e23f
Nusab Taha <nusabtaha33@gmail.com>
2023-09-30 04:13:41 +0600

    test grid

--- app/credits/page.jsx
+++ app/credits/page.jsx
@@ -1,84 +1,107 @@
 import NavBar from "@/components/NavBar";
 import Footer from "@/components/Footer";
-import Link from "next/link";
 import Image from "next/image";
+import Link from "next/link";
+
+const EXTERNAL = (
+  <svg
+    className="mb-2 ml-2 inline-block h-4 w-4"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 18 18"
+  >
+    <path
+      stroke="currentColor"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      strokeWidth="2"
+      d="M15 11v4.833A1.166 1.166 0 0 1 13.833 17H2.167A1.167 1.167 0 0 1 1 15.833V4.167A1.166 1.166 0 0 1 2.167 3h4.618m4.447-2H17v5.768M9.111 8.889l7.778-7.778"
+    />
+  </svg>
+);
+
+const MakeLink = (text, link, target) => {
+  if (!target) target = "_self";
+  return (
+    <Link
+      href={link}
+      className="roundedLg mx-1 rounded-lg px-1 py-1 font-bold text-sky-500 transition-all duration-200 hover:bg-gray-800 hover:bg-opacity-60 hover:text-emerald-500"
+      target={target}
+    >
+      <span>
+        {text}
+        {target === "_blank" ? EXTERNAL : ""}
+      </span>
+    </Link>
+  );
+};
 
 const page = () => {
+  const profiles = [
+    {
+      name: "John Doe",
+      profession: "Web Developer",
+      description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      image: "/assets/images/ahn.jpg",
+    },
+    {
+      name: "Jane Smith",
+      profession: "UI/UX Designer",
+      description:
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      image: "/assets/images/ahn.jpg",
+    },
+    {
+      name: "Bob Johnson",
+      profession: "Backend Developer",
+      description:
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      image: "/assets/images/ahn.jpg",
+    },
+    {
+      name: "Alice Williams",
+      profession: "Frontend Developer",
+      description:
+        "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      image: "/assets/images/ahn.jpg",
+    },
+  ];
+
   return (
     <>
       <NavBar />
-      <div className="py-15 container mx-auto mb-20 mt-10 flex w-11/12 flex-col items-center justify-center px-5 text-center tracking-wide md:w-10/12">
-        <h1 className="mb-8 text-3xl font-bold text-gray-100 md:text-5xl">
-          Credits
-        </h1>
-        <p className="mb-5 text-base text-gray-200 md:text-lg">
-          Creating this website with Next.js and Tailwind CSS was a bit like
-          trying to assemble a furniture without the instructions - a few
-          stumbles and a lot of{" "}
-          <q>
-            <i>what does this part even do?</i>
-          </q>{" "}
-          moments.
-          <br />
-          But hey, even the best chefs use recipes, right? I took help from some
-          cool resources to make this project.
-          <br />
-          <br />
-          Here are my secret weapons that made this project possible. 🚀
-        </p>
-        <div className="mb-14 mt-5 flex flex-col text-base text-gray-200 md:text-lg">
-          <Link
-            href="https://flowbite.com"
-            target="_blank"
-            className="title-font mb-3 text-2xl font-bold text-teal-400 hover:text-indigo-400"
-          >
-            <Image
-              src="/assets/images/flowbite.svg"
-              width={30}
-              height={30}
-              alt="Flowbite Icon"
-              className="mb-1 mr-2 inline w-7"
-            />
-            Flowbite
-          </Link>
-          <span>
-            I went on a bit of a component shopping🛒 spree at Flowbite,
-            grabbing awesome stuff like the <b>Navbar</b>, <b>Footer</b>, and
-            some snazzy <b>SVG icons</b>. You could say it was a component heist
-            of epic proportions! 💎
-            <br />
-            <br />
-            But here's where it gets interesting. While I borrowed these goodies
-            from Flowbite, I had to sprinkle some of my own coding magic to make
-            them responsive. I mean, who needs a <b>flowbite.js</b> wizard when
-            you've got your own skills? 😎
-            <br />
-          </span>
+      <div className="mb-20 px-10 py-10 text-gray-300">
+        <header className="text-4xl font-semibold text-white md:text-6xl">
+          People Who Made This Possible
+        </header>
+
+        <div className="textLg mt-10 md:text-xl">
+          <p className="mt-5">
+            These are the people who made this website possible. Without them,
+            it would have been a mess.
+          </p>
         </div>
-        {/* tailblock */}
-        <div className="flex flex-col text-base text-gray-200 md:text-lg">
-          <Link
-            href="https://tailblocks.cc/"
-            target="_blank"
-            className="title-font mb-3 text-2xl font-bold text-teal-400 hover:text-indigo-400"
-          >
-            <Image
-              src="/assets/images/tailblock.png"
-              width={30}
-              height={30}
-              alt="Tailblock Icon"
-              className="mb-1 mr-2 inline w-7"
-            />
-            Tailblock
-          </Link>
-          <span>
-            Tailblock, another gem of a website!
-            <br />I snatched the{" "}
-            <Link href="/#why-us" className="font-bold text-blue-400">
-              <q>Why Contest Hive?</q>
-            </Link>{" "}
-            section straight from Tailblock. 😉🌟
-          </span>
+
+        <div className="mt-10 grid grid-cols-2 gap-4">
+          {profiles.map((profile, index) => (
+            <div key={index} className="overflow-hidden rounded-lg bg-gray-800">
+              <div className="flex h-40 items-center justify-center">
+                <Image
+                  width={100}
+                  height={100}
+                  className="rounded-full"
+                  src={profile.image}
+                  alt={profile.name}
+                />
+              </div>
+              <div className="px-6 py-4">
+                <div className="mb-2 text-xl font-bold">{profile.name}</div>
+                <p className="text-base text-gray-300">{profile.profession}</p>
+                <p className="mt-2 text-base text-gray-400">
+                  {profile.description}
+                </p>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
       <Footer />
@@ -87,3 +110,20 @@ const page = () => {
 };
 
 export default page;
+
+const title = "Credits | Contest Hive";
+const description = "Credits page of Contest Hive. Who made this possible?";
+const keywords =
+  "contest hive credits, contest hive, contest api credits, upcoming contest credits, codeforces api, toph api, kontests api, hackerearth api, hackerrank api, codechef api, atcoder api, leetcode api, nusab taha, nusab19, toph leaderboard";
+const url = "https://contest-hive.vercel.app/credits";
+
+export const metadata = {
+  title: title,
+  description: description,
+  keywords: keywords,
+  openGraph: {
+    title: title,
+    description: description,
+    url: url,
+  },
+};
