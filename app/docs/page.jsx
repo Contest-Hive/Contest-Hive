11470d045ee64fc35da20aaa6d8492cfd2241535
Nusab Taha <nusabtaha33@gmail.com>
2023-12-18 01:21:30 +0600

    Delete unused files and update middleware

--- app/docs/page.jsx
+++
@@ -1,7 +0,0 @@
-import Content from "@/components/docs/Content";
-
-const page = () => {
-  return <Content />;
-};
-
-export default page;
--- middleware.js
+++ middleware.js
@@ -1,11 +1,18 @@
 import { NextResponse } from "next/server";
 import GenerateRssFeed from "@/components/GenerateRssFeed";
 
+const docsUrl = "https://contest-hive.github.io/docs";
+
 export async function middleware(req) {
   const { nextUrl } = req;
   const origin = String(nextUrl.origin) + "/";
   const href = String(nextUrl.href);
 
+  if (req.nextUrl.pathname.startsWith("/docs")) {
+    const path = req.nextUrl.pathname.slice(5);
+    const targetUrl = docsUrl + path;
+    return NextResponse.redirect(targetUrl);
+  }
 
   // return rss feed
   if (href.toLowerCase().endsWith("/rss.xml")) {
@@ -17,14 +24,13 @@ export async function middleware(req) {
     });
   }
 
-
   // Exclude some paths
   const excludedValues = ["_next", "favicon", "assets", "icon", "image"];
 
   for (const value of excludedValues) {
     if (href.toLowerCase().includes(value)) return NextResponse.next();
   }
-  
+
   const path = href.toLowerCase().includes("api") ? "api" : "page";
 
   async function makeReq() {
