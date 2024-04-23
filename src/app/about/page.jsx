ec18f49608993f8d7bce93a78ad86941e3eae6e2
Nusab Taha <nusabtaha33@gmail.com>
2024-04-24 03:36:49 +0600

    Fix alignment issue in About page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const About = () => {
   return (
-    <header className="text-2xl text-center">Meow Meow</header>
+    <header className="text-center">Meow Meow</header>
   )
 }
 
