cd632f72c318e18f8e8fc91f78e53759d1e322ed
Nusab Taha <nusabtaha33@gmail.com>
2024-04-27 01:05:28 +0600

    Refactor Slider component to use useRef hook and add MaxWidthWrapper component

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import { Recursive } from "next/font/google";
 
 import Balancer from "react-wrap-balancer";
-import { GitHubLogoIcon } from "@radix-ui/react-icons";
+import { GitHubLogoIcon, ReaderIcon } from "@radix-ui/react-icons";
 import { Button } from "@/components/ui/button";
 
 import {
@@ -20,58 +20,53 @@ const fontRecursive = Recursive({
   variable: "--font-sans",
 });
 
-const GITHUB_Link = ""
-
+const GITHUB_Link = "https://github.com/Contest-Hive/Contest-Hive";
 
 const Hero = () => {
   return (
     <div
       className={cn(
         fontRecursive.className,
-        "grainy-dark mx-auto min-h-screen w-full max-w-screen-2xl px-2.5 dark:bg-none md:px-20",
+        "grainy-dark mx-auto w-full px-2.5 py-10 dark:bg-none md:px-20",
       )}
     >
       <header className="pb-8 pt-20 text-center font-heading text-4xl font-bold leading-tight tracking-tighter md:text-5xl lg:text-6xl">
-        <span className="mr-1.5 rounded-lg bg-pinkish px-2 py-1 text-secondary dark:text-primary">
+        <span className="mr-1.5 rounded-lg bg-pinkish px-2 pb-1 text-secondary dark:text-primary">
           Contest Hive
         </span>
         is All You Need
       </header>
       <p className="mx-auto max-w-lg text-center text-sm md:max-w-xl md:text-base lg:max-w-2xl lg:text-lg">
-        <Balancer>
-          <span className="font-semibold">
-            Keeping track of upcoming contests is a{" "}
-            <TooltipProvider>
-              <Tooltip>
-                <TooltipTrigger className="hidden cursor-help border-b-2 border-dashed border-primary font-bold md:inline-block">
-                  Spain.
-                </TooltipTrigger>
-                <TooltipContent className="max-w-52 py-2 text-xs lg:text-sm">
-                  <a
-                    href="https://en.wikipedia.org/wiki/Sulfur"
-                    target="_blank"
-                    className="border-b-2 border-dotted border-black text-lg font-bold"
-                    title="Open Wikipedia of Sulfur"
-                  >
-                    <sup className="text-xs">16</sup>S
-                  </a>{" "}
-                  is silent (⌐■_■)
-                </TooltipContent>
-              </Tooltip>
-            </TooltipProvider>
-            <span className="inline-block border-b-2 border-dashed border-primary font-bold md:hidden">
-              pain.
-            </span>
+        <span className="font-semibold">
+          Keeping track of upcoming contests is a{" "}
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger className="hidden cursor-help border-b-2 border-dashed border-primary font-bold md:inline-block">
+                Spain.
+              </TooltipTrigger>
+              <TooltipContent className="max-w-52 py-2 text-xs lg:text-sm">
+                <a
+                  href="https://en.wikipedia.org/wiki/Sulfur"
+                  target="_blank"
+                  className="border-b-2 border-dotted border-black text-lg font-bold"
+                  title="Open Wikipedia of Sulfur"
+                >
+                  <sup className="text-xs">16</sup>S
+                </a>{" "}
+                is silent (⌐■_■)
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+          <span className="inline-block border-b-2 border-dashed border-primary font-bold md:hidden">
+            pain.
           </span>
-          <br />
-          <span className="font-semibold tracking-tighter">
-            Not anymore! 🚀
-          </span>
-          <br />
-          Contest Hive will keep you updated with all the upcoming contests.
-        </Balancer>
+        </span>
+        <br />
+        <span className="font-semibold tracking-tighter">Not anymore! 🚀</span>
+        <br />
+        Contest Hive will keep you updated with all the upcoming contests.
       </p>
-      <div className="flex items-center justify-center py-10">
+      <div className="flex items-center justify-center gap-2 py-10">
         <Button asChild>
           <Link
             href={GITHUB_Link}
@@ -79,12 +74,21 @@ const Hero = () => {
             target="_blank"
           >
             <GitHubLogoIcon />
-            See Contests
+            Github
+          </Link>
+        </Button>
+        <Button asChild variant="outline">
+          <Link
+            href={GITHUB_Link}
+            className="flex items-center justify-center gap-2"
+            target="_blank"
+          >
+            <ReaderIcon />
+            API Docs
           </Link>
         </Button>
       </div>
       <Slider />
-
     </div>
   );
 };
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,25 +1,71 @@
+import { useRef } from "react";
+
 import Image from "next/image";
 import Marquee from "react-fast-marquee";
+
+import MaxWidthWrapper from "./MaxWidthWrapper";
+import { cn } from "@/lib/utils";
 
 const images =
   "atcoder codeforces leetcode codechef toph hackerrank hackerearth".split(" ");
 
 const Slider = () => {
+  const marqueeRef = useRef(null);
+
   return (
-    <Marquee autoFill>
-      {images.map((image) => (
-        <div key={image} className="flex items-center flex-col justify-center font-semibold">
-          <Image
-            src={`/assets/svgs/platforms/transparent/${image}.svg`}
-            alt={image}
-            width={1}
-            height={1}
-            className="mx-2 h-24 w-24 rounded-md p-4 dark:bg-primary"
-          />
-          <p>{image[0].toUpperCase()+image.slice(1)}</p>
-        </div>
-      ))}
-    </Marquee>
+    <MaxWidthWrapper>
+      <div
+        className="flex select-none overflow-hidden py-4 md:py-10"
+        ref={marqueeRef}
+      >
+        {images.map((image) => (
+          <div
+            key={image}
+            className={cn(
+              "flex flex-col items-center justify-center font-semibold grayscale transition-all duration-300 ease-in-out hover:grayscale-0",
+              images.indexOf(image) % 2 ? "hover:-rotate-6" : "hover:rotate-6",
+            )}
+          >
+            <Image
+              src={`/assets/svgs/platforms/transparent/${image}.svg`}
+              alt={image}
+              width={1}
+              height={1}
+              className="mx-10 h-16 w-16 rounded-md p-4 dark:bg-primary md:h-24 md:w-24"
+            />
+            <p>{image[0].toUpperCase() + image.slice(1)}</p>
+          </div>
+        ))}
+      </div>
+      <Marquee
+        autoFill
+        play={false}
+        className="select-none overflow-hidden py-4 md:py-10"
+        onMount={() => {
+          // remove `marqueeRef.current` from the DOM
+          marqueeRef.current.remove();
+        }}
+      >
+        {images.map((image) => (
+          <div
+            key={image}
+            className={cn(
+              "flex flex-col items-center justify-center font-semibold grayscale transition-all duration-300 ease-in-out hover:grayscale-0",
+              images.indexOf(image) % 2 ? "hover:-rotate-6" : "hover:rotate-6",
+            )}
+          >
+            <Image
+              src={`/assets/svgs/platforms/transparent/${image}.svg`}
+              alt={image}
+              width={1}
+              height={1}
+              className="mx-10 h-16 w-16 rounded-md p-4 dark:bg-primary md:h-24 md:w-24"
+            />
+            <p>{image[0].toUpperCase() + image.slice(1)}</p>
+          </div>
+        ))}
+      </Marquee>
+    </MaxWidthWrapper>
   );
 };
 
