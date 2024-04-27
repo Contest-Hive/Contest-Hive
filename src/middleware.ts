8783d7beb5bddba99a710048253fe90f4630019f
Nusab Taha <nusabtaha33@gmail.com>
2024-04-27 16:33:32 +0600

    middleware

---
+++ src/middleware.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const docsUrl = "https://contest-hive.github.io/docs";
+
+export async function middleware(req: NextRequest) {
+  const { nextUrl } = req;
+  const origin = String(nextUrl.origin) + "/";
+  const href = String(nextUrl.href);
+
+  if (req.nextUrl.pathname.startsWith("/docs")) {
+    const path = req.nextUrl.pathname.slice(5);
+    const targetUrl = docsUrl + path;
+    return NextResponse.redirect(targetUrl);
+  }
+
+  // return rss feed
+  //   if (href.toLowerCase().endsWith("/rss.xml")) {
+  //     const xml = await GenerateRssFeed();
+  //     return new Response(xml, {
+  //       headers: {
+  //         "Content-Type": "application/xml;charset=UTF-8",
+  //       },
+  //     });
+  //   }
+
+  // Exclude some paths
+  const excludedValues = ["_next", "favicon", "assets", "icon", "image"];
+
+  for (const value of excludedValues) {
+    if (href.toLowerCase().includes(value)) return NextResponse.next();
+  }
+
+  const path = href.toLowerCase().includes("api") ? "api" : "page";
+
+  async function makeReq() {
+    await fetch(`${origin}/api/others/stats`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ path }), // send the path as it is shorter than the full URL
+    });
+  }
+
+  await makeReq();
+  console.log("middleware done:", href);
+  return NextResponse.next();
+}
