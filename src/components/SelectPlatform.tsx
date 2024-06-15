29a3e495827b0c078cada9f8dcdf46055fd8d95b
Nusab Taha <nusabtaha33@gmail.com>
2024-06-15 18:16:21 +0600

    change the layout to be more engaging

--- src/components/SelectPlatform.tsx
+++ src/components/SelectPlatform.tsx
@@ -33,33 +33,40 @@ export default function SelectPlatform({
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="sm" className="h-10 gap-1">
-          <ListFilter className="h-3.5 w-3.5" />
+        <Button variant="outline" size="sm" className="h-10 select-none gap-1">
+          {/* <ListFilter className="h-3.5 w-3.5" /> */}
+          <Image
+            src={`/assets/svgs/platforms/${platform.includes("Gym") ? "codeforces" : platform.toLocaleLowerCase()}.svg`}
+            alt="Platform Logo"
+            width={1}
+            height={1}
+            className="h-5 w-5 rounded-sm"
+          />
           <span className="sr-only sm:not-sr-only sm:whitespace-nowrap">
             Platform
           </span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="start">
-          <DropdownMenuLabel>Select Platform</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          {PLATFORMS.map((plt) => (
-            <DropdownMenuCheckboxItem
-              key={plt}
-              checked={plt === platform}
-              onClick={() => setPlatform(plt)}
-              className="flex items-center justify-start gap-2 text-xs md:text-sm"
-            >
-              <Image
-                src={`/assets/svgs/platforms/${plt.includes("Gym") ? "codeforces" : plt.toLocaleLowerCase()}.svg`}
-                alt="Platform Logo"
-                width={1}
-                height={1}
-                className="h-5 w-5 rounded-sm"
-              />
-              {plt}
-            </DropdownMenuCheckboxItem>
-          ))}
+        <DropdownMenuLabel>Select Platform</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {PLATFORMS.map((plt) => (
+          <DropdownMenuCheckboxItem
+            key={plt}
+            checked={plt === platform}
+            onClick={() => setPlatform(plt)}
+            className="flex items-center justify-start gap-2 text-xs md:text-sm"
+          >
+            <Image
+              src={`/assets/svgs/platforms/${plt.includes("Gym") ? "codeforces" : plt.toLocaleLowerCase()}.svg`}
+              alt="Platform Logo"
+              width={1}
+              height={1}
+              className="h-5 w-5 rounded-sm"
+            />
+            {plt}
+          </DropdownMenuCheckboxItem>
+        ))}
       </DropdownMenuContent>
     </DropdownMenu>
   );
