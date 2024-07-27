81f2aeb86e3a7e2b0cfe4f9128a1eb20c3972175
Nusab Taha <nusabtaha33@gmail.com>
2024-07-28 03:02:44 +0600

    change `keyboard shortcut` button icon

--- src/components/sub/KeyboardShortcuts.tsx
+++ src/components/sub/KeyboardShortcuts.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "../ui/separator";
 import {
   Card,
   CardContent,
@@ -6,6 +7,7 @@ import {
   CardFooter,
   CardTitle,
 } from "../ui/card";
+
 
 const SHORTCUTS = [
   {
@@ -32,18 +34,21 @@ const SHORTCUTS = [
 
 export default function KeyboardShortcuts() {
   const defaultClass =
-    "pointer-events-none inline-flex h-5 select-none items-center rounded border bg-muted px-1.5 font-mono font-medium text-primary text-sm";
+    "pointer-events-none inline-flex h-5 select-none items-center rounded border bg-muted px-1.5 font-mono font-medium text-primary/80 text-sm";
 
   return (
     <Card className="border-none p-1">
-      <CardHeader className="rounded-md bg-muted-foreground p-2 text-center font-semibold text-primary-foreground">
+      <CardHeader className="rounded-md bg-muted p-2 text-center mt-1 font-bold text-primary">
         Keyboard Shortcuts
       </CardHeader>
       <CardTitle className="hidden">Keyboard Shortcuts</CardTitle>
       <CardContent className="px-2 py-2 md:py-3">
         {SHORTCUTS.map(({ key, followedByKey, description }) => (
-          <div key={key} className="my-1 flex items-center gap-2 text-primary">
-            <span className="flex min-w-28 items-center justify-around gap-1">
+          <div
+            key={key}
+            className="my-1.5 flex h-6 items-center justify-start gap-2 text-primary"
+          >
+            <span className="flex min-w-28 items-center justify-between gap-2 pr-2">
               <kbd className={defaultClass}>{key}</kbd>
               {followedByKey && (
                 <>
@@ -52,7 +57,8 @@ export default function KeyboardShortcuts() {
                 </>
               )}
             </span>
-            <CardDescription className="font-mono">
+            <Separator orientation="vertical" className="h-4 bg-primary/50" />
+            <CardDescription className="text-primary/80">
               {description}
             </CardDescription>
           </div>
