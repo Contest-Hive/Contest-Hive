a671a159db2880cabb63440da6a85451e04c3020
Nusab Taha <nusabtaha33@gmail.com>
2024-08-24 16:50:33 +0600

    change `heart` svg

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,7 +11,19 @@ const Footer = () => {
       <Separator />
       <div className="container mx-auto px-4 pb-14 pt-6">
         <p className="text-center text-sm text-gray-500">
-          Made with ❤️ by{" "}
+          Made with{" "}
+          <svg
+            className="animate-bounce inline h-5 w-5 md:h-6 md:w-6 text-sky-600 dark:text-blue-500"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M927.4 273.5v-95.4h-87.9V82.8h-201v95.3h-87.9v95.4h-78.5v-95.4h-88V82.8H183.2v95.3H95.3v95.4H16.7v190.6h78.6v95.4h75.3v95.3H246v95.3h87.9v95.4h100.5v95.3h153.9v-95.3h100.4v-95.4h88v-95.3H852.1v-95.3h75.3v-95.4h78.5V273.5z"
+              fill="currentColor"
+            />
+          </svg>{" "}
+          by{" "}
           <a
             href={PORTFOLIO_URL}
             target="_blank"
