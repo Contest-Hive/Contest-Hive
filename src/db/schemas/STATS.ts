0e77bbd603b8a911c49e17b714ad6daf9c0e0907
Nusab Taha <nusabtaha33@gmail.com>
2025-04-17 23:39:26 +0600

    fix typescript problems

--- src/db/schemas/STATS.ts
+++ src/db/schemas/STATS.ts
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
 
-const StatsSchema = new mongoose.Schema({
+export interface Stats {
+  _id: number;
+  api: number;
+  page: number;
+  total: number;
+  past24: number;
+  past24api: number;
+  past24page: number;
+}
+
+const StatsSchema = new mongoose.Schema<Stats>({
   _id: { type: Number },
   api: { type: Number },
   page: { type: Number },
@@ -10,5 +20,5 @@ const StatsSchema = new mongoose.Schema({
   past24page: { type: Number },
 });
 
-export default mongoose.models.Stats ||
-  mongoose.model("Stats", StatsSchema, "contest-hive");
+export default (mongoose.models.Stats as mongoose.Model<Stats>) || 
+  mongoose.model<Stats>("Stats", StatsSchema, "contest-hive");
